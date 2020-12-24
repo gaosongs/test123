a5d9ec6e5a2b309aef0484d5c49c28e1dd84ac46
高嵩 <18846114771@163.com>
2020-12-24 20:33:12 +0800

    TestGit2

--- src/main/java/com/ruoyi/RuoYiApplication.java
+++ src/main/java/com/ruoyi/RuoYiApplication.java
@@ -16,6 +16,8 @@ public class RuoYiApplication
     {
 
 
+
+
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
